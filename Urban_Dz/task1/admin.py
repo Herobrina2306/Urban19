de639158714f5400f5f98a03562d57486177529d
AleksandraM <musatova.aleksandra@mail.ru>
2025-01-26 12:38:52 +0300

    module19_4

--- Urban_Dz/task1/admin.py
+++ Urban_Dz/task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_filter = ('size', 'cost')
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'balanse', 'age')
+    search_fields = ('name', )
+    list_filter = ('balanse', 'age')
+    readonly_fields = ('balanse',)
+    list_per_page = 30
